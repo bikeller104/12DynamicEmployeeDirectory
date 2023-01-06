5b43febfc79f05e0dafff81e3ae71bed125f81a7
Brent Keller <bikeller104@gmail.com>
2023-01-06 12:00:15 -0700

    added add role functionality

--- db_functions/sqlFunctions.js
+++ db_functions/sqlFunctions.js
@@ -14,7 +14,7 @@ class Database {
         });
     }  
     //view all departments- this shows a list of departments
-    getDepartments()
+    getDepartments(callback)
     {
         this.db.query(`select name from departments`, (err, results) => {
             if(err) 
@@ -23,10 +23,20 @@ class Database {
                 return;
             }
             console.table(results);
+            callback();
         })
     }
+    //pass the list of departments to a callback function 
+    //ill use this to pass an inquirer function to the call back so 
+    //it can ask the user to choose a department from a list
+    getDepartmentList(callbackWithResults) {
+        this.db.query('select name from departments', (err, results) => {
+            callbackWithResults(results.map(result => result.name));
+        })
+    }
+
     //view all roles - this shows a list of roles
-    getRoles()
+    getRoles(callback)
     {
         const query = `select r.id, r.title, r.salary, d.name as 'department' from roles r Right Join departments d on r.department_id = d.id;`;
         this.db.query(query, (err, results) => {
@@ -36,11 +46,22 @@ class Database {
                 return;
             }
             console.table(results);
+            callback();
+        })
+    }
+
+    getDepartmentRow(departmentName)
+    {
+        const query = `Select * From departments WHERE name = ?`;
+        this.db.query(query, departmentName, (err, results) => {
+            console.table(results);
+            console.log(results);
+            console.log(results[0].id);
         })
     }
     
     //view all employees - this shows a table of employees with their roles and managers
-    getEmployees()
+    getEmployees(callback)
     {
         const query = `Select e.first_name, e.last_Name, r.title, r.salary, d.name as 'department' , CONCAT( e2.first_name, ' ', e2.last_name) as 'manager' from  employees e  Join roles r  ON e.role_id = r.id   join departments d  on r.department_id = d.id Left Join employees e2  On e.manager_id = e2.id`;
 
@@ -51,18 +72,61 @@ class Database {
                 return;
             }
             console.table(results);
+            callback();
         });
        
     }
 
-
     //add a department- this adds an entry to the department table
+    addDepartment(departmentName, callback)
+    {
+        const query = `INSERT INTO  departments (name) Values (?)`;
+        this.db.query(query, departmentName, (err, results) => 
+        {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            callback();
+        });
+    }
+
+    
 
     //add a role -this adds an entry to the role table takes a title, a salary, and a role/role id
+    addRole(roleName, salary, departmentName, callback)
+    {
+
+        const query = `Select * From departments WHERE name = ?`;
+        this.db.query(query, departmentName, (err, results) => {
+
+            const query = `INSERT INTO roles(title, salary, department_id) Values (?, ?, ?)`;
+            this.db.query(query, [roleName, salary, results[0].id], (err, res) =>
+            {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                callback();
+        });
+    });
+    }
+
+    addEmployee(firstName, lastName, roleName, managerName)
+    {
+
+    }
+
+    updateEmployee() {}
+
+
     
 
 
-    close = () => this.db.end();
+    close = () => {
+        this.db.end();
+        console.log(`database closed`);
+    }
 }
 
 module.exports = Database;
--- index.js
+++ index.js
@@ -1,41 +1,118 @@
 const  sqlParams = require('./sql_config');
 const mysql = require('mysql2');
-
+const inquirer = require('inquirer');
 
 const Database = require('./db_functions/sqlFunctions');
 
 const db = new Database(sqlParams.sqlConParams);
 
-const tables = {
-    employees: "employees",
-    departments: "departments",
-    roles: "roles"
-}
+// const tables = {
+//     employees: "employees",
+//     departments: "departments",
+//     roles: "roles"
+// }
 
 const startOptions = [
-    "view all departmests",
+    "view all departments",
     "view all roles",
     "view all employees",
     "add a department",
     "add a role",
     "add an employee", 
-    "update an employee role"
-
+    "update an employee role",
+    "quit"
 ]
 
-console.log("Select all for departments");
-db.selectAllQuery(tables.departments);
-console.log("Select all for employees");
-db.selectAllQuery(tables.employees);
-console.log("Select all for roles");
-db.selectAllQuery(tables.roles);
+const startQuestions = [
+    {
+        name:'baseResponse',
+        type: 'list',
+        message:'What do you want to do',
+        choices: startOptions,
+    }
+]
 
-console.log("Select all for departments");
-db.getDepartments();
-console.log("Select all for roles");
-db.getRoles();
-console.log("Select all join all tables");
-db.getEmployees();
+const getDepartmentInput = [
+{
+    name: 'department',
+    type: 'input',
+    message: 'Enter the new department name',
+}
+]
+
+const getRoleInput = [
+    {
+        name: 'role',
+        type: 'input',
+        message: 'Enter the new role name',
+    },
+    {
+        name: 'salary',
+        type: 'input',
+        message: 'Enter the new salary',
+    }
+]
+
+function addRolewithDepartmentInput(departmentsList)
+{
+    const roleInput = [ 
+        ...getRoleInput,
+        {
+            name: 'department',
+            type: 'list',
+            message: 'Choose the new department the role is in',
+            choices: departmentsList,
+        }
+     ]
 
 
-db.close();
+     inquirer.prompt(roleInput).then(responses =>
+    {
+        db.addRole(responses.role, responses.salary, responses.department, askQuestions);
+    });
+}
+
+function askQuestions()
+{
+    inquirer.prompt(startQuestions)
+    .then((responses) => 
+    {
+        console.log(responses);
+        switch(responses.baseResponse)
+        {
+            case "view all departments":
+                db.getDepartments(askQuestions);
+                console.log("got departments");
+                break;
+            case "view all roles":
+                db.getRoles(askQuestions);
+                //askQuestions();
+                break;
+            case "view all employees":
+                db.getEmployees(askQuestions);
+                //askQuestions();
+                break;
+            case "add a department": 
+                inquirer.prompt(getDepartmentInput)
+                .then((response) =>
+                {
+                    db.addDepartment(response.department);
+                });
+                break;
+            case "add a role":
+                db.getDepartmentList(addRolewithDepartmentInput)
+                break;
+            case "add an employee": 
+                
+                break;
+            case "update an employee role":
+
+                break;
+            default:
+                console.log("ending program");
+                break;
+            }
+    });
+}
+
+askQuestions();
